8d07bd49aca030dfa27e92ab3bdd418bdc6b2e41
chrisalexander <chris@chris-alexander.co.uk>
2013-02-23 16:15:08 +0000

    Lists and editing their names

--- js/list.js
+++ js/list.js
@@ -1,3 +1,22 @@
 var list = function(ko) {
 	
+	//********** Private methods
+	
+	//********** Private variables
+	
+	//********** Public methods
+	
+	//********** Public variables
+	
+	// The name of this list
+	var name = ko.observable("Untitled List");
+	
+	// Whether or not this list's name is being edited
+	var editingName = ko.observable(false);
+	
+	return {
+		name: name,
+		editingName: editingName
+	}
+	
 };
--- js/lists.js
+++ js/lists.js
@@ -2,23 +2,42 @@ var lists = (function(ko, list) {
 	
 	//********** Private methods
 	
+	// Helper to create a new list with the correct dependencies
+	function getNewList(name) {
+		var l = new list(ko);
+		if (name) {
+			l.name(name);
+		}
+		return l;
+	}
+	
 	//********** Private variables
 	
-	var currentList = ko.observable(-1)
+	// The currently viewing list ID
+	var currentListId = ko.observable(-1);
+	
+	// Subscribe to the ID to set the correct context
+	currentListId.subscribe(function(id) {
+		if (id < 0) {
+			currentList(false);
+		} else {
+			currentList(lists()[id]);
+		}
+	});
 	
 	//********** Public methods
 	
 	// Shortcut to go edit a list
 	function editList(id) {
+		currentListId(id);
 		view("edit");
-		currentList(id);
 	}
 	
 	// Shortut to create a list
 	function newList() {
-		lists.push(new list(ko));
-		view("edit")
-		currentList(lists.length)
+		lists.push(getNewList());
+		currentListId(lists().length-1);
+		view("edit");
 	}
 	
 	//********** Public variables
@@ -26,14 +45,25 @@ var lists = (function(ko, list) {
 	// Which view is currently active
 	var view = ko.observable("home");
 	
+	// Blank the current list on going home
+	view.subscribe(function(v) {
+		if (v == "home") {
+			currentListId(-1);
+		}
+	})
+	
 	// The list of shopping lists
-	var lists = ko.observableArray([{"name": "derp"}]);
+	var lists = ko.observableArray([getNewList("My First List")]);
+	
+	// The current list we are editing
+	var currentList = ko.observable(false);
 	
 	return {
 		view: view,
 		lists: lists,
 		editList: editList,
-		newList: newList
+		newList: newList,
+		currentList: currentList
 	}
 	
 })(ko, list)
